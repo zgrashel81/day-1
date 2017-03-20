afac5bf2d70afdc78a141a92bc8e70f4c7b0580b
Zachary Grashel <zgrashel@yahoo.com>
2017-03-20 17:41:34 -0400

    statistic adventure

--- user-input-statistics/app.rb
+++ user-input-statistics/app.rb
@@ -1,20 +1,16 @@
-puts "Enter a number: "
-number1 = gets.chomp.to_f
-
-puts "Enter another number: "
-number2 = gets.chomp.to_f
-
-puts "Enter yet another number: "
-number3 = gets.chomp.to_f
-
-puts "Enter yet another number still: "
-number4 = gets.chomp.to_f
-
-puts "Enter the final number: "
-number5 = gets.chomp.to_f
-
-total = number1 + number2 + number3 + number4 + number5.to_f
-
-average = total /5
-
-puts "Your total number is #{total} and the average is #{average}!"
+average = 0
+count = 0
+total = 0
+loop do
+  puts "Enter numbers!"
+  input = gets.chomp
+if input == ""
+    puts "Thank you, your total is {#{total} and you average is #{average}!"
+    break
+else
+    total = total + input.to_f
+    count = count + 1
+    average = total / count
+    puts "Your total is #{total} and your average is #{average}"
+end
+end
